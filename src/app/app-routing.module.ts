377431f5f94bfdd71e02dc1d897c77c212a3e61f
Habibur Rahman <habiburrahman-mu@users.noreply.github.com>
2022-03-06 17:21:24 +0600

    Initial ui design for todo app

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {TodosComponent} from "./todos/todos.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+    {
+        path: '',
+        component: TodosComponent
+    }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TodosComponent } from './todos/todos.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TodosComponent
   ],
   imports: [
     BrowserModule,
