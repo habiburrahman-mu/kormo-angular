7e2fa5bb206bc4184a64d2a0b2be7a4b0037bde0
Habibur Rahman <habiburrahman-mu@users.noreply.github.com>
2022-03-07 22:48:27 +0600

    services added for todo list

---
+++ src/app/shared/services/data.service.ts
@@ -0,0 +1,31 @@
+import {Injectable} from '@angular/core';
+import {ToDo} from "../model/todo.model";
+
+@Injectable({
+    providedIn: 'root'
+})
+export class DataService {
+    todos: ToDo[] = [
+        new ToDo('This is a test'),
+        new ToDo('Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam non finibus augue. Morbi nec mauris urna. Mauris fermentum rutrum convallis. Pellentesque non gravida elit.')
+    ]
+    constructor() {
+    }
+
+    getAllTodos() {
+        return this.todos;
+    }
+
+    addTodo(todo: ToDo) {
+        this.todos.push(todo);
+    }
+
+    updateTodo(index: number, updatedTodo: ToDo) {
+        this.todos[index] = updatedTodo;
+    }
+
+    deleteTodo(index: number) {
+        this.todos.splice(index, 1);
+    }
+
+}
--- src/app/todos/todos.component.ts
+++ src/app/todos/todos.component.ts
@@ -1,15 +1,20 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {ToDo} from "../shared/model/todo.model";
+import {DataService} from "../shared/services/data.service";
 
 @Component({
-  selector: 'app-todos',
-  templateUrl: './todos.component.html',
-  styleUrls: ['./todos.component.scss']
+    selector: 'app-todos',
+    templateUrl: './todos.component.html',
+    styleUrls: ['./todos.component.scss']
 })
 export class TodosComponent implements OnInit {
+    todos: ToDo[]
+    constructor(private dataService: DataService) {
 
-  constructor() { }
+    }
 
-  ngOnInit(): void {
-  }
+    ngOnInit(): void {
+        this.todos = this.dataService.getAllTodos();
+    }
 
 }
